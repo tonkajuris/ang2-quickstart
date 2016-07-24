50795724c035735e92250be1ce7a1a9e67dd714d
tonkajuris <tonkajuris@gmail.com>
2016-07-23 21:12:03 -0500

    Adding files to repo

---
+++ app/hero.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+  getHeroes() {
+    return Promise.resolve(HEROES);
+  }
+  getHeroesSlowly() {
+      return new Promise<Hero[]>(resolve =>
+        setTimeout(() => resolve(HEROES), 10000) // 2 seconds
+        );
+  }
+  getHero(id: number) {
+      return this.getHeroes()
+        .then(heroes => heroes.find(hero => hero.id === id));
+  }
+}
